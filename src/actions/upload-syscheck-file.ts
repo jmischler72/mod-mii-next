cbec893a614238bdaad82c24a7a20fe04790a105
Joffrey <joffrey.mischlerps@gmail.com>
2025-09-08 17:13:04 +0200

    chore(syscheck): group syscheck logic

--- src/actions/upload-syscheck-file.ts
+++ src/actions/upload-syscheck-file.ts
@@ -1,29 +1,10 @@
 'use server';
 
-import {
-	checkD2XCios,
-	checkExtraProtection,
-	checkForMissingIOS,
-	checkIfBootMiiInstalled,
-	checkIfHBCIsOutdated,
-	checkIfPriiloaderInstalled,
-	translateKeywordsToEnglish,
-	validateConsoleType,
-	validateSyscheckData,
-} from '@/helpers/syscheck-validation-helper';
-import { UploadSyscheckResult } from '@/types/upload-syscheck-type';
-import {
-	getConsoleRegion,
-	getConsoleType,
-	getFirmware,
-	getHBCVersion,
-	getLatestSMVersion,
-	getRegionShortCode,
-	getSystemMenuVersion,
-} from '@/helpers/syscheck-info-helper';
 import { CustomError } from '@/types/custom-error';
 import { getDatabaseEntry } from '@/helpers/database-helper';
 import { uploadFormDataSchema } from '@/schemas/upload-schema';
+import { UploadSyscheckResult } from '@/types/upload-syscheck-type';
+import { getSyscheckAnalysis } from '@/helpers/syscheck/syscheck-analysis';
 
 export async function uploadSyscheckFile(formData: FormData): Promise<UploadSyscheckResult> {
 	try {
@@ -51,16 +32,7 @@ export async function uploadSyscheckFile(formData: FormData): Promise<UploadSysc
 			throw new CustomError('The CSV file appears to be empty');
 		}
 
-		// copy csv to copydata
-		let copyData = csvContent;
-
-		copyData = translateKeywordsToEnglish(copyData);
-
-		if (!validateSyscheckData(copyData)) {
-			throw new CustomError('The CSV file is not a valid SysCheck report');
-		}
-
-		const systemInfos = handleSyscheckData(copyData, { activeIOS, extraProtection, cMios });
+		const systemInfos = getSyscheckAnalysis(csvContent, { activeIOS, extraProtection, cMios });
 
 		const wadsInfos = systemInfos.wadToInstall.map((wadId) => {
 			return { wadname: getDatabaseEntry(wadId)?.wadname || 'Unknown', wadId };
@@ -88,96 +60,4 @@ export async function uploadSyscheckFile(formData: FormData): Promise<UploadSysc
 			error: error instanceof CustomError ? error.message : 'An error occurred while processing the syscheck file',
 		};
 	}
-}
-
-function handleSyscheckData(
-	data: string,
-	options: { activeIOS?: boolean; extraProtection?: boolean; cMios?: boolean },
-) {
-	options.extraProtection = false; // Extra protection is disabled for now
-
-	const region = getConsoleRegion(data);
-	const hbcVersion = getHBCVersion(data);
-	const systemMenuVersion = getSystemMenuVersion(data);
-	const firmware = systemMenuVersion && getFirmware(systemMenuVersion);
-	const consoleType = getConsoleType(data);
-
-	if (!region || !systemMenuVersion || !firmware) {
-		throw new CustomError('Could not extract necessary information from the CSV file');
-	}
-
-	if (!consoleType || !validateConsoleType(consoleType)) {
-		throw new CustomError('The CSV file does not contain a valid console type');
-	}
-
-	const regionShortCode = getRegionShortCode(region);
-	if (firmware.SMregion !== regionShortCode) {
-		throw new CustomError(
-			`The firmware region "${firmware.SMregion}" does not match the console region "${regionShortCode}"`,
-		);
-	}
-
-	const wadToInstall = [];
-
-	// Check system components
-	const isBootMiiInstalled = checkIfBootMiiInstalled(data);
-	const isPriiloaderInstalled = checkIfPriiloaderInstalled(data);
-	const isHbcOutdated = hbcVersion ? checkIfHBCIsOutdated(hbcVersion, consoleType) : false;
-	const outdatedD2XCios = checkD2XCios(data, consoleType);
-	const missingIOS = options.activeIOS ? checkForMissingIOS(data, region, consoleType) : [];
-	const needsExtraProtection = options.extraProtection ? checkExtraProtection(data) : [];
-
-	if (!isBootMiiInstalled) {
-		wadToInstall.push('HM');
-	} else {
-		if (!hbcVersion) {
-			wadToInstall.push('OHBC113');
-			//also check if IOS58 is installed
-			const isIOS58Installed = data.includes('IOS58');
-			if (!isIOS58Installed && isBootMiiInstalled) wadToInstall.push('IOS58');
-		} else {
-			if (isHbcOutdated) wadToInstall.push('OHBC');
-		}
-	}
-
-	const latestFirmwareVersion = getLatestSMVersion(firmware);
-	if (latestFirmwareVersion !== firmware.firmware) wadToInstall.push(`SM${latestFirmwareVersion}${firmware.SMregion}`);
-
-	const updatePriiloader = false;
-	if (!isPriiloaderInstalled || (isPriiloaderInstalled && updatePriiloader)) wadToInstall.push('prii');
-
-	wadToInstall.push(...outdatedD2XCios);
-
-	if (options.activeIOS) {
-		wadToInstall.push(...missingIOS);
-	}
-
-	if (options.extraProtection) {
-		wadToInstall.push(...needsExtraProtection);
-	}
-
-	if (wadToInstall.length > 0) {
-		wadToInstall.push('yawm');
-	}
-
-	return {
-		region,
-		hbcVersion,
-		systemMenuVersion,
-		firmware: {
-			SMregion: firmware.SMregion,
-			firmware: firmware.firmware,
-			firmwareVersion: firmware.firmwareVersion,
-		},
-		consoleType,
-		systemChecks: {
-			isBootMiiInstalled,
-			isPriiloaderInstalled,
-			isHbcOutdated,
-			missingIOS,
-			outdatedD2XCios,
-			needsExtraProtection,
-		},
-		wadToInstall,
-	};
 }
---
+++ src/helpers/syscheck/syscheck-analysis.ts
@@ -0,0 +1,119 @@
+
+import {
+    checkD2XCios,
+    checkExtraProtection,
+    checkForMissingIOS,
+    checkIfBootMiiInstalled,
+    checkIfHBCIsOutdated,
+    checkIfPriiloaderInstalled,
+    translateKeywordsToEnglish,
+    validateConsoleType,
+    validateSyscheckData,
+} from '@/helpers/syscheck/validation-helper';
+import {
+    getConsoleRegion,
+    getConsoleType,
+    getFirmware,
+    getHBCVersion,
+    getLatestSMVersion,
+    getRegionShortCode,
+    getSystemMenuVersion,
+} from '@/helpers/syscheck/info-helper';
+import { CustomError } from '@/types/custom-error';
+
+export function getSyscheckAnalysis(
+	data: string,
+	options: { activeIOS?: boolean; extraProtection?: boolean; cMios?: boolean },
+) {
+    data = translateKeywordsToEnglish(data);
+
+    if (!validateSyscheckData(data)) {
+        throw new CustomError('The CSV file is not a valid SysCheck report');
+    }
+	options.extraProtection = false; // Extra protection is disabled for now
+
+	const region = getConsoleRegion(data);
+	const hbcVersion = getHBCVersion(data);
+	const systemMenuVersion = getSystemMenuVersion(data);
+	const firmware = systemMenuVersion && getFirmware(systemMenuVersion);
+	const consoleType = getConsoleType(data);
+
+	if (!region || !systemMenuVersion || !firmware) {
+		throw new CustomError('Could not extract necessary information from the CSV file');
+	}
+
+	if (!consoleType || !validateConsoleType(consoleType)) {
+		throw new CustomError('The CSV file does not contain a valid console type');
+	}
+
+	const regionShortCode = getRegionShortCode(region);
+	if (firmware.SMregion !== regionShortCode) {
+		throw new CustomError(
+			`The firmware region "${firmware.SMregion}" does not match the console region "${regionShortCode}"`,
+		);
+	}
+
+	const wadToInstall = [];
+
+	// Check system components
+	const isBootMiiInstalled = checkIfBootMiiInstalled(data);
+	const isPriiloaderInstalled = checkIfPriiloaderInstalled(data);
+	const isHbcOutdated = hbcVersion ? checkIfHBCIsOutdated(hbcVersion, consoleType) : false;
+	const outdatedD2XCios = checkD2XCios(data, consoleType);
+	const missingIOS = options.activeIOS ? checkForMissingIOS(data, region, consoleType) : [];
+	const needsExtraProtection = options.extraProtection ? checkExtraProtection(data) : [];
+
+	if (!isBootMiiInstalled) {
+		wadToInstall.push('HM');
+	} else {
+		if (!hbcVersion) {
+			wadToInstall.push('OHBC113');
+			//also check if IOS58 is installed
+			const isIOS58Installed = data.includes('IOS58');
+			if (!isIOS58Installed && isBootMiiInstalled) wadToInstall.push('IOS58');
+		} else {
+			if (isHbcOutdated) wadToInstall.push('OHBC');
+		}
+	}
+
+	const latestFirmwareVersion = getLatestSMVersion(firmware);
+	if (latestFirmwareVersion !== firmware.firmware) wadToInstall.push(`SM${latestFirmwareVersion}${firmware.SMregion}`);
+
+	const updatePriiloader = false;
+	if (!isPriiloaderInstalled || (isPriiloaderInstalled && updatePriiloader)) wadToInstall.push('prii');
+
+	wadToInstall.push(...outdatedD2XCios);
+
+	if (options.activeIOS) {
+		wadToInstall.push(...missingIOS);
+	}
+
+	if (options.extraProtection) {
+		wadToInstall.push(...needsExtraProtection);
+	}
+
+	if (wadToInstall.length > 0) {
+		wadToInstall.push('yawm');
+	}
+
+	return {
+		region,
+		hbcVersion,
+		systemMenuVersion,
+		firmware: {
+			SMregion: firmware.SMregion,
+			firmware: firmware.firmware,
+			firmwareVersion: firmware.firmwareVersion,
+		},
+		consoleType,
+		systemChecks: {
+			isBootMiiInstalled,
+			isPriiloaderInstalled,
+			isHbcOutdated,
+			missingIOS,
+			outdatedD2XCios,
+			needsExtraProtection,
+		},
+		wadToInstall,
+	};
+}
